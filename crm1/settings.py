43d48e7e67a25b6afa8483107eb2b631ecfa7051
Joy Debnath <51055647+jooydebnath@users.noreply.github.com>
2021-12-27 22:42:13 +0600

    Update settings.py

--- crm1/settings.py
+++ crm1/settings.py
@@ -84,16 +84,16 @@ WSGI_APPLICATION = 'crm1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASE['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASE['default'].update(db_from_env)
 
 
 
